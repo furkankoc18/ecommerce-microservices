2973b730d032e85fd37c4337c1c78e5beae25e70
Furkan KOÇ <furkankoc@192.168.1.86>
2025-11-16 11:10:29 +0300

    first commit

---
+++ product-service/src/main/java/com/furkan/productservice/controller/ProductController.java
@@ -0,0 +1,33 @@
+package com.furkan.productservice.controller;
+
+import com.furkan.productservice.dto.ProductRequest;
+import com.furkan.productservice.dto.ProductResponse;
+import com.furkan.productservice.model.Product;
+import com.furkan.productservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/product")
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductRequest productRequest){
+        productService.createProduct(productRequest);
+
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts(){
+        return productService.getAllProducts();
+    }
+
+}
